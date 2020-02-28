31768a3f2915a2c8d06893fa041be3e801d950ef
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-28 15:06:25 +1100

    Snapshot testing

--- src/components/card.test.js
+++ src/components/card.test.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import Card from './Card';
 
+
+// Snapshot, only update the snapshot to pass if the change is 100% by pressing 'u' key for update
 it('expect to render Card component', () => {
-  expect(shallow(<Card />).length).toEqual(1);
+  expect(shallow(<Card />)).toMatchSnapshot();
 });
